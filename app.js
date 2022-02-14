b75ab3fdb12a75769483cf99a9ffcb60f68109fa
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-02-14 13:27:17 -0800

    created objects in js

---
+++ app.js
@@ -0,0 +1,39 @@
+
+
+// Site Objects
+
+const locSeattle = {
+  location: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgSales: 6.3,
+}
+
+const locTokyo = {
+  location: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgSales: 1.2,
+}
+
+const locDubai = {
+  location: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgSales: 3.7,
+}
+
+const locParis = {
+  location: 'Paris',
+  minCust: 20,
+  maxCust: 38,
+  avgSales: 2.3,
+}
+
+const licLima = {
+  location: 'Lima',
+  minCust: 2,
+  maxCust: 16,
+  avgSales: 4.6,
+}
+
