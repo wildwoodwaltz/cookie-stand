425184fcf97b51b58082515e1df8df5fe2d36bf7
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-02-15 15:34:22 -0800

    Finished grand totals for hour and daily

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // //setting variable
 let hoursOfOperation = ['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00AM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM'];
 let storeLocations = [];
+let hourlyTotal =[];
 let sales = document.getElementById('sales');
 
 //Constructor for location objects.
@@ -59,7 +60,7 @@ Storelocation.prototype.render = function(){
  
 
 //initial table header creation
-function createTable(){
+function renderTableHeader(){
 const theadElement = document.createElement('thead')
 sales.appendChild(theadElement);
 const trElement = document.createElement('tr');
@@ -77,14 +78,42 @@ tableTotalDaily.textContent = `Total Daily Cookie Sales`;
 trElement.appendChild(tableTotalDaily);
 }
 
+function renderTableFooter(){
+const tfootElement = document.createElement('tfoot')
+sales.appendChild(tfootElement);
+const hourlyTotalTable = document.createElement('th')
+hourlyTotalTable.textContent = `Hourly Totals`
+tfootElement.appendChild(hourlyTotalTable)
+  //calculate each instance of cookies
+    //Run once for each hour of operation
+  let grandDailyTotal = 0
+    for (let i = 0 ; i < hoursOfOperation.length; i++) {
+    //run for each store and as per above, for every hour
+    let grandHourTotal = 0;
+    for (let j = 0; j < storeLocations.length; j++){
+      // nested allows it to run through every store during particular hour.
+      grandHourTotal += (storeLocations[j].cookiesPerHour[i]);
+    }
+    const tdGrandHourTotal = document.createElement('td')
+    //after each instance report grand hourly total to table
+    tdGrandHourTotal.textContent = `${grandHourTotal}`
+    tfootElement.appendChild(tdGrandHourTotal);  
+    //add grand hour total to grand daily total until operation done.
+    grandDailyTotal += grandHourTotal;
+    }
+  const tdGrandDailyTotal = document.createElement ('td');
+  tdGrandDailyTotal.textContent = `${grandDailyTotal}`;
+  tfootElement.appendChild(tdGrandDailyTotal);
+}
 
 //called function to run everything
 function calcStoreSales() {
-  createTable();
+  renderTableHeader();
   for ( let i = 0; i < storeLocations.length; i++) {
     let getStores = storeLocations[i];
     getStores.render();
   }
+  renderTableFooter();
 }
 
   calcStoreSales();
